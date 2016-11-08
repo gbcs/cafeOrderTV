acff524a0bfcee38e08ffefea4614c384956a309
Gary Barnett <gbarnett@garys-imac.local>
2016-11-07 18:16:43 -0800

    order close UI

--- cafeOrderTV/ViewController.swift
+++ cafeOrderTV/ViewController.swift
@@ -13,17 +13,12 @@ class ViewController: UIViewController, UICollectionViewDelegate, UICollectionVi
     @IBOutlet var collectionView: UICollectionView!
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-       // collectionView.register(OrderCell.self, forCellWithReuseIdentifier: Constants.orderCollectionViewReuseIdentifier )
       
         collectionView.delegate = self
         collectionView.dataSource = self
         
         NotificationCenter.default.addObserver(self, selector: #selector(updateOrdersView), name: NSNotification.Name(rawValue: Constants.ordersUpdatedNotification), object: nil)
         NotificationCenter.default.addObserver(self, selector: #selector(updateOOOSign), name: NSNotification.Name(rawValue: Constants.ordersAPIOutOfOrder), object: nil)
-        
-        
-        
         
     }
 
@@ -47,38 +42,43 @@ class ViewController: UIViewController, UICollectionViewDelegate, UICollectionVi
         return 1
     }
     
-    
+
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-    
         return 10
     }
 
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
-        
-        // get a reference to our storyboard cell
         let cell = collectionView.dequeueReusableCell(withReuseIdentifier: Constants.orderCollectionViewReuseIdentifier, for: indexPath as IndexPath) 
         
         let timeLabel:UILabel = cell.viewWithTag(1) as! UILabel
         let tableSeatLabel:UILabel = cell.viewWithTag(2) as! UILabel
         let orderTextLabel:UILabel = cell.viewWithTag(3) as! UILabel
-       // let orderFocusOverlay:UILabel = cell.viewWithTag(4) as! UILabel
-        
-        
-
-        
+      
         timeLabel.text = "timel"
         tableSeatLabel.text = "tableseat"
         orderTextLabel.text = "this is order text"
         
-
         return cell
     }
    
-    // MARK: - UICollectionViewDelegate protocol
     
     func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
-        // handle tap events
         print("You selected cell #\(indexPath.item)!")
+        
+        let controller:UIAlertController = UIAlertController.init(title: "Close Order", message: "Is this order complete?", preferredStyle: .actionSheet)
+    
+        controller.addAction(UIAlertAction.init(title: "Close Order", style: .default, handler: { (action) in
+            //Make api call to close order.
+            //If successful, refresh orders, causing a UI refresh
+        }));
+        
+        controller.addAction(UIAlertAction.init(title: "Keep Order Open", style: .cancel, handler: { (action) in
+            
+        }));
+        
+        self.present(controller, animated: true) {
+            
+        }
     }
     
      func collectionView(_ collectionView: UICollectionView, shouldSelectItemAt indexPath: IndexPath) -> Bool {
@@ -116,25 +116,6 @@ class ViewController: UIViewController, UICollectionViewDelegate, UICollectionVi
             collectionView.scrollToItem(at: indexPath, at: [.centeredHorizontally, .centeredVertically], animated: true)
         }
     }
-    
-    
-    
-    /*
-     // Uncomment these methods to specify if an action menu should be displayed for the specified item, and react to actions performed on the item
-     override func collectionView(_ collectionView: UICollectionView, shouldShowMenuForItemAt indexPath: IndexPath) -> Bool {
-     return false
-     }
-     
-     override func collectionView(_ collectionView: UICollectionView, canPerformAction action: Selector, forItemAt indexPath: IndexPath, withSender sender: Any?) -> Bool {
-     return false
-     }
-     
-     override func collectionView(_ collectionView: UICollectionView, performAction action: Selector, forItemAt indexPath: IndexPath, withSender sender: Any?) {
-     
-     }
-     */
-
-    
     
 }
 
